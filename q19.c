de4846249a4c4632463909c7fcfc7bf52b068133
ljdaosm <39611504+ljdaosm@users.noreply.github.com>
2020-11-24 10:16:36 +0530

    Add files via upload

---
+++ q19.c
@@ -0,0 +1,11 @@
+//program to print ASCII (google)
+#include<stdio.h>
+int main()
+{
+    int x;
+    for (x=0;x<=255;x++)
+    {
+        printf("ASCII values of character %c=%d\n",x,x);
+    }
+    return 0;
+}
