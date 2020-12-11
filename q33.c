7e0f48ff5e72f9448b78c7991449e95dba1373b9
ljdaosm <39611504+ljdaosm@users.noreply.github.com>
2020-12-11 20:15:02 +0530

    Add files via upload

---
+++ q33.c
@@ -0,0 +1,29 @@
+//find perfect numbers till n
+#include <stdio.h>
+int main()
+{
+    int i, j, n, sum;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    printf("All Perfect numbers between 1 to %d:\n", n);
+    
+    for(i=1; i<=n; i++)
+    {
+        sum = 0;
+        for(j=1; j<i; j++)
+        {
+            if(i % j == 0)
+            {
+                sum += j;
+            }
+        }
+ 
+        if(sum == i)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
